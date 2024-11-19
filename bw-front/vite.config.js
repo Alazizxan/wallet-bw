a23aaa39655014b75564aa4c40a9b1a6e09e9c4b
Al Azizxan <123247928+Alazizxan@users.noreply.github.com>
2024-11-19 16:39:49 +0500

    Update vite.config.js

--- bw-front/vite.config.js
+++ bw-front/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Tarmoqda mavjud bo'lishi uchun
+    port: 3000, // Portni belgilash (masalan, 3000)
+  },
 })
